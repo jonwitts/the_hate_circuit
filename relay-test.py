787ed7b24a1691660c3554f04c6b0a52e2a05604
Jon Witts <jonwitts@gmail.com>
2026-08-23 18:05:40 +0100

    toggle function

--- relay-test.py
+++ relay-test.py
@@ -1,14 +1,55 @@
 import RPi.GPIO as GPIO
 import time
 
-def relay_trigger(relay_int, low_high, duration, debug = False):
+# def relay_trigger(relay_int, low_high, duration, debug = False):
+#     '''
+#     wire positive between A and C on relay
+#     A to power, C to device
+
+#     relay_int = 5, 6, 13, 16, 19, 20, 21, 26
+#     low_high = low OR high
+#     time = time in action in ms
+#     debug = True OR False, defaults to False
+#     '''
+
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+
+#     Relay = [5, 6, 13, 16, 19, 20, 21, 26]
+
+#     if debug:
+#         print("setup relays")
+#     for i in range(len(Relay)):
+#         GPIO.setup(Relay[i], GPIO.OUT)
+#         GPIO.output(Relay[i], GPIO.HIGH)
+
+#     if debug:
+#         print("checking relay_int")
+#     if relay_int in Relay:
+#         if debug:
+#             print("checking low_high")
+#         if low_high == "low":
+#             if debug:
+#                 print("set relay to low")
+#             GPIO.output(relay_int, GPIO.LOW)
+#             time.sleep(duration)
+#         elif low_high == "high":
+#             if debug:
+#                 print("set relay to high")
+#             GPIO.output(relay_int, GPIO.HIGH)
+#             time.sleep(duration)
+
+#     if debug:
+#         print("GPIO cleanup")
+#     GPIO.cleanup()
+
+def relay_toggle(relay_int, on_off, debug = False):
     '''
     wire positive between A and C on relay
     A to power, C to device
 
     relay_int = 5, 6, 13, 16, 19, 20, 21, 26
-    low_high = low OR high
-    time = time in action in ms
+    on_off = on OR off
     debug = True OR False, defaults to False
     '''
 
@@ -27,21 +68,13 @@ def relay_trigger(relay_int, low_high, duration, debug = False):
         print("checking relay_int")
     if relay_int in Relay:
         if debug:
-            print("checking low_high")
-        if low_high == "low":
-            if debug:
-                print("set relay to low")
+            print("toggle relay")
+        if on_off == "on":
             GPIO.output(relay_int, GPIO.LOW)
-            time.sleep(duration)
-        elif low_high == "high":
-            if debug:
-                print("set relay to high")
+        elif on_off == "off":
             GPIO.output(relay_int, GPIO.HIGH)
-            time.sleep(duration)
 
-    if debug:
-        print("GPIO cleanup")
-    GPIO.cleanup()
+print("Manual trigger")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -52,7 +85,7 @@ for i in range(len(Relay)):
         GPIO.setup(Relay[i], GPIO.OUT)
         GPIO.output(Relay[i], GPIO.HIGH)
 
-while True:
+for i in range(5):
     key = input("Enter 1 to switch on, 0 to switch off and q to exit:\n")
     if key == "1":
         GPIO.output(5, GPIO.LOW)
@@ -63,3 +96,13 @@ while True:
         break
     else:
         print("Please enter 1 or 0")
+
+print(" Using toggle function")
+
+for i in range(5):
+    print("On for 5 seconds")
+    relay_toggle(5, "on", debug = True)
+    time.sleep(5)
+    print("Off for 5 seconds")
+    relay_toggle(5, "off", debug = True)
+    time.sleep(5)
